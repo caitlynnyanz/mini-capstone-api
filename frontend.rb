a064f7432f79bd04cc041f07410cea1f110bf221
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-06-28 19:30:26 -0600

    added TTY-Table to frontend

--- frontend.rb
+++ frontend.rb
@@ -1,7 +1,22 @@
 require "http"
+require "tty-table"
 
 system "clear"
 
+table = TTY::Table.new(["Title", "Author", "Price"], [])
+
 response = HTTP.get("http://localhost:3000/all_products")
 
-pp JSON.parse(response.body)
+data = JSON.parse(response.body)
+index = 0
+
+while index < data.length
+  title = data[index]["title"]
+  author = data[index]["author"]
+  price = data[index]["price"]
+
+  table << [title, author, price]
+  index = index + 1
+end
+
+puts table.render(:ascii)
