f1231bc72142a5f9228270295f69643fc5b83a1b
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-06-28 18:30:09 -0600

    added model and controller

---
+++ db/migrate/20220629002815_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.string :author
+      t.integer :price
+      t.string :image_url
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
