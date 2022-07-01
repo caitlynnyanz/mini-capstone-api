a94ac4b0f7ee436401dcba3069cd1b3b2f1ff88f
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-06-30 19:56:55 -0600

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,24 @@
 class Product < ApplicationRecord
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def is_discounted?
+    if price > 10
+      return false
+    elsif price < 10
+      return true
+    end
+  end
+
+  def tax
+    tax = price * 0.06
+    return tax
+  end
+
+  def total
+    tax = price * 0.06
+    total = price + tax
+    return total
+  end
 end
