d87a24ab84b10957c9221cf99a96c668f4333ae8
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-06 19:48:41 -0600

    added show and index views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    products = Product.all
-    render json: products.as_json
+    @products = Product.all
+    render template: "products/index"
   end
 
   def show
-    product_id = params["id"]
-    product = Product.find_by(id: product_id)
-    render json: product.as_json(methods: [:friendly_created_at, :is_discounted?, :tax, :total])
+    @product = Product.find_by(id: params[:id])
+    render template: "products/show"
   end
 
   def create
@@ -17,8 +16,9 @@ class ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description],
     )
+    @product = product
     if product.save
-      render json: product.as_json
+      render template: "products/show"
     else
       render json: { errors: product.errors.full_messages }, status: 422
     end
@@ -33,8 +33,9 @@ class ProductsController < ApplicationController
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
 
+    @product = product
     if product.save
-      render json: product.as_json
+      render template: "products/show"
     else
       render json: { errors: product.errors.full_messages }, status: 422
     end
