6dc816ea44e8c1a24ea76a6eaae145fe0ba8abb4
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-10 13:02:21 -0600

    added image model and attached to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :description, presence: true, length: { in: 10..500 }
 
   belongs_to :supplier
+  has_many :images
   # def supplier
   #   Supplier.find_by(id: supplier_id)
   # end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,26 @@
 
 # Product.create(name: "'The Picture of Dorian Gray' by Oscar Wilde", price: 14.99, image_url: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781625587534/the-picture-of-dorian-gray-9781625587534_hr.jpg", description: "The story revolves around a portrait of Dorian Gray painted by Basil Hallward, a friend of Dorian's and an artist infatuated with Dorian's beauty..")
 
-Supplier.create(name: "Oxford World's Classics", email: "oxfordclassics@test.com", phone_number: "1234567889")
+# Supplier.create(name: "Oxford World's Classics", email: "oxfordclassics@test.com", phone_number: "1234567889")
 
-Supplier.create(name: "Penguin Classic", email: "penguinclassics@test.com", phone_number: "987654321")
+# Supplier.create(name: "Penguin Classic", email: "penguinclassics@test.com", phone_number: "987654321")
 
-Supplier.create(name: "Barnes & Noble Classics", email: "barnesandnoble@test.com", phone_number: "123654789")
+# Supplier.create(name: "Barnes & Noble Classics", email: "barnesandnoble@test.com", phone_number: "123654789")
+
+Image.create(url: "https://images-na.ssl-images-amazon.com/images/I/71CcO-jvRUL.jpg", product_id: 1)
+
+Image.create(url: "https://images-na.ssl-images-amazon.com/images/I/A1aDb5U5myL.jpg", product_id: 2)
+
+Image.create(url: "https://static.wikia.nocookie.net/classical-literature/images/e/e9/Wuthering-heights1.jpg/revision/latest?cb=20190503012621", product_id: 3)
+
+Image.create(url: "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781625587534/the-picture-of-dorian-gray-9781625587534_hr.jpg", product_id: 4)
+
+Image.create(url: "https://i.pinimg.com/originals/b7/31/30/b73130b6d298ccc99f886e6821bf5077.jpg", product_id: 1)
+
+Image.create(url: "https://www.gohd.com.sg/wp-content/uploads/2014/08/warandpeace-easton7.JPG", product_id: 2)
+
+Image.create(url: "https://upload.wikimedia.org/wikipedia/commons/6/64/Houghton_Lowell_1238.5_%28A%29_-_Wuthering_Heights%2C_1847.jpg", product_id: 3)
+
+Image.create(url: "https://www.bl.uk/britishlibrary/~/media/bl/global/dl%20romantics%20and%20victorians/collection-items-manual/w/i/l/wilde%20oscar%201891%20b20145-23.jpg", product_id: 4)
 
 puts "Done!"
