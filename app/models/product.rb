463c2ceedae93e3d8bae214c9cbc2744f51298d4
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-10 11:00:06 -0600

    added methods to see supplier with products and vice versa

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true, length: { in: 10..500 }
 
+  def supplier
+    Supplier.find_by(id: supplier_id)
+  end
+
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
   end
