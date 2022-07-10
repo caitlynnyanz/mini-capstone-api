6045d21b742088b1c0dab2263c309acb57a68ac2
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-10 11:13:48 -0600

    updated to shorthand: belongs_to and has_many

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -3,7 +3,8 @@ class Supplier < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :phone_number, presence: true, uniqueness: true
 
-  def products
-    Product.where(supplier_id: id)
-  end
+  has_many :products
+  # def products
+  #   Product.where(supplier_id: id)
+  # end
 end
