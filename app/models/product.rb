6a6bef045661ade596ed1b7d9fdfaee72287911c
Caitlynn Yanz <caitlynnyanz601@gmail.com>
2022-07-13 20:29:27 -0600

    added categories, join table, and has_many : through

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,17 @@ class Product < ApplicationRecord
   validates :description, presence: true, length: { in: 10..500 }
 
   belongs_to :supplier
+  has_many :category_products
   has_many :images
   has_many :orders
   # def supplier
   #   Supplier.find_by(id: supplier_id)
+  # end
+  has_many :categories, through: :category_products
+  # def categories
+  #   category_products.map do |category_product|
+  #     category_product.category
+  #   end
   # end
 
   def friendly_created_at
